0e82dd40f9e1f04b4a3702798f0beabcf732bfe4
ZenoTan <zenotan1998@gmail.com>
2020-08-26 16:26:57 +0800

    balance

--- bench.go
+++ bench.go
@@ -1,6 +1,15 @@
 package main
 
-import "time"
+import (
+	"context"
+	"fmt"
+	"github.com/prometheus/common/model"
+	"os"
+	"time"
+
+	"github.com/prometheus/client_golang/api"
+	v1 "github.com/prometheus/client_golang/api/prometheus/v1"
+)
 
 type bench interface {
 	run() error
@@ -21,18 +30,63 @@ func (s *scaleOut) run() error {
 	if err := s.c.addStores(1); err != nil {
 		return err
 	}
-	for {
-		time.Sleep(time.Minute)
-		if s.isBalance() {
-			return nil
-		}
-	}
+	//for {
+	//	//	time.Sleep(time.Minute)
+	//	//	if s.isBalance() {
+	//	//		return nil
+	//	//	}
+	//	//}
+	s.isBalance()
+	return nil
 }
 
 func (s *scaleOut) isBalance() bool {
 	// todo get data from prometheus
 	// todo @zeyuan
-	return false
+	client, err := api.NewClient(api.Config{
+		Address: s.c.prometheus,
+	})
+	if err != nil {
+		fmt.Printf("Error creating client: %v\n", err)
+		os.Exit(1)
+	}
+
+	v1api := v1.NewAPI(client)
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	r := v1.Range{
+		Start: time.Now().Add(-9 * time.Minute),
+		End:   time.Now(),
+		Step:  time.Minute,
+	}
+	result, warnings, err := v1api.QueryRange(ctx, "pd_scheduler_store_status{type=\"region_score\"}", r)
+	if err != nil {
+		fmt.Printf("Error querying Prometheus: %v\n", err)
+		os.Exit(1)
+	}
+	if len(warnings) > 0 {
+		fmt.Printf("Warnings: %v\n", warnings)
+	}
+	matrix := result.(model.Matrix)
+	for _, data := range matrix {
+		if len(data.Values) != 10 {
+			return false
+		}
+		mean := 0.0
+		dev := 0.0
+		for _, v := range data.Values {
+			mean += float64(v.Value) / 10
+		}
+		for _, v := range data.Values {
+			dev += (float64(v.Value) - mean) * (float64(v.Value) - mean) / 10
+		}
+		if mean * mean * 0.1 < dev {
+			return false
+		}
+	}
+	fmt.Printf("Result:\n%v\n", result)
+	fmt.Println("Balanced")
+	return true
 }
 
 func (s *scaleOut) collect() error {
